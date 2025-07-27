3c4c0fecbde2e8fd329d192765c02dd0d34c84ab
Ivan Jilev <jilev09@gmail.com>
2025-07-27 09:20:13 +0300

    About page

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DropBoxMarket.Models;
+
+public class ContactController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]  
+    public IActionResult Index(ContactFormModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        TempData["Message"] = "Thank you for your message! We'll get back to you soon.";
+        return RedirectToAction("Index");
+    }
+}
---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DropBoxMarket.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Message { get; set; }
+    }
+}
