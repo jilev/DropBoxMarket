5e5ab6675065ee79155c4f0b4ee72b0a8929a692
Ivan Jilev <jilev09@gmail.com>
2025-07-29 19:55:03 +0300

    Fixed some bugs with CRUD operations

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -33,6 +33,7 @@ namespace DropBoxMarket.Areas.Admin.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Product product, IFormFile? imageFile)
         {
             if (!ModelState.IsValid)
@@ -44,7 +45,12 @@ namespace DropBoxMarket.Areas.Admin.Controllers
             if (imageFile != null && imageFile.Length > 0)
             {
                 string uploadsFolder = Path.Combine(_env.WebRootPath, "images");
-                string uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
                 using (var stream = new FileStream(filePath, FileMode.Create))
@@ -72,6 +78,8 @@ namespace DropBoxMarket.Areas.Admin.Controllers
         }
 
         [HttpPost]
+
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Product product, IFormFile? imageFile)
         {
             if (!ModelState.IsValid)
@@ -80,10 +88,18 @@ namespace DropBoxMarket.Areas.Admin.Controllers
                 return View(product);
             }
 
+            var existingProduct = _context.Products.AsNoTracking().FirstOrDefault(p => p.Id == product.Id);
+            if (existingProduct == null) return NotFound();
+
             if (imageFile != null && imageFile.Length > 0)
             {
                 string uploadsFolder = Path.Combine(_env.WebRootPath, "images");
-                string uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
                 using (var stream = new FileStream(filePath, FileMode.Create))
@@ -92,6 +108,11 @@ namespace DropBoxMarket.Areas.Admin.Controllers
                 }
 
                 product.ImageUrl = "/images/" + uniqueFileName;
+            }
+            else
+            {
+
+                product.ImageUrl = existingProduct.ImageUrl;
             }
 
             _context.Products.Update(product);
@@ -114,6 +135,15 @@ namespace DropBoxMarket.Areas.Admin.Controllers
             var product = await _context.Products.FindAsync(id);
             if (product == null) return NotFound();
 
+            if (!string.IsNullOrEmpty(product.ImageUrl))
+            {
+                var filePath = Path.Combine(_env.WebRootPath, product.ImageUrl.TrimStart('/'));
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+            }
+
             _context.Products.Remove(product);
             await _context.SaveChangesAsync();
 
@@ -121,16 +151,12 @@ namespace DropBoxMarket.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+
+
         public IActionResult Details(int id)
         {
             var product = _context.Products.Include(p => p.Category).FirstOrDefault(p => p.Id == id);
             return product == null ? NotFound() : View(product);
-        }
-
-        public IActionResult All()
-        {
-            var products = _context.Products.Include(p => p.Category).ToList();
-            return View(products);
         }
     }
 }
