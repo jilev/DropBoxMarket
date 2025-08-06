dde53ecfd391784fd233433aeb58e24139aab77f
Ivan Jilev <jilev09@gmail.com>
2025-08-06 10:18:07 +0300

    Changes in Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using DropBoxMarket.Data;
 using DropBoxMarket.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,34 +8,50 @@ namespace DropBoxMarket.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        private void SetCategoryCount()
+        {
+            ViewBag.CategoryCount = _context.Categories.Count();
         }
 
         public IActionResult Index()
         {
+            SetCategoryCount();
             return View();
         }
 
         public IActionResult Privacy()
         {
+            SetCategoryCount();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            SetCategoryCount();
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            SetCategoryCount();
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public IActionResult About() => View();
 
         //public IActionResult TestError()
         //{
         //    throw new Exception("Test 500 error");
         //}
-
     }
+
+
 }
